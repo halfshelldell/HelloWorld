42e5b19995b56139fc7462c2dbfe23e2bba38260
halfshelldell <simmonslydell@gmai.com>
2016-05-19 10:08:47 -0400

    added notes

---
+++ src/HelloWorld.java
@@ -0,0 +1,41 @@
+/**
+ * Created by illladell on 5/18/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+/*
+String[] names = new String[3];
+//String Array
+names[0] = "Alice";
+names[1] = "Bob";
+names[2] = "Charlie";
+names
+
+for(String name : names) {
+    System.out.println(name);
+}
+
+int i = 0;
+while (i < 5) {
+    System.out.println("Hello");
+    i +=1;
+}
+
+// Primitive
+int score = 1;
+double health = 100.0;
+boolean isAlive = true;
+
+// Class (Boxed)
+String name =  "Alice";
+Person p = new Person();
+Integer.valueOf("123");
+Double.valueOf("10.5");
+// Boxed Class Examples
+Integer score = 1;
+Double health = 100.0;
+Boolean isAlive = true;
+*/
