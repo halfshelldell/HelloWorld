e2ddf3da4a6bbe32c42798dc3056fada2cde634a
halfshelldell <simmonslydell@gmai.com>
2016-05-23 10:21:16 -0400

    add exercise

---
+++ src/Exercise01.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by illladell on 5/23/16.
+ */
+public class Exercise01 {
+    public static void main(String[] args) {
+        String[] names = {"Alice", "Bob", "Charlie", "David", "Andrea", "Albert", "Beth"};
+        // "A": ["Alice", "Andrea", "Albert"]
+        // "B": ["Bob", "Beth"]
+        // "C": ["Charlie"]
+        // "D": ["David"]
+
+        HashMap<String, ArrayList<String>> nameMap = new HashMap<>();
+
+        // for loop better for arrays
+
+        for (String name : names) {
+            char firstLetter = name.charAt(0);
+            String firstLetterStr = String.valueOf(firstLetter);
+            nameMap.put(firstLetterStr, new ArrayList<>());
+        }
+
+        // nameMap currently looks like this:
+        // "A": []
+        // "B": []
+        // "C": []
+        // "D": []
+
+        for (String name : names) {
+            char firstLetter = name.charAt(0);
+            String firstLetterStr = String.valueOf(firstLetter);
+            ArrayList<String> arr = nameMap.get(firstLetterStr);
+            arr.add(name);
+        }
+        System.out.println(nameMap);
+
+        // alternate solution (better)
+
+        nameMap = new HashMap<>();
+
+        for (String name : names) {
+            char firstLetter = name.charAt(0);
+            String firstLetterStr = String.valueOf(firstLetter);
+            ArrayList<String> arr = nameMap.get(firstLetterStr);
+            // arr could be null
+            if (arr == null) {
+                arr = new ArrayList<>();
+                arr.add(name);
+                nameMap.put(firstLetterStr, arr);
+            }
+            else {
+                arr.add(name);
+            }
+        }
+        System.out.println(nameMap);
+    }
+
+}
